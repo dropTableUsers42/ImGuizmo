107702f1d9cfd8258293ba2069390fa9b28f27a9
dropTableUsers42 <rwitaban0001@gmail.com>
2022-05-18 01:54:32 +0530

    viewManipulate now returns whether it's being used

--- ImGuizmo.cpp
+++ ImGuizmo.cpp
@@ -2669,7 +2669,7 @@ namespace IMGUIZMO_NAMESPACE
       }
    }
 
-   void ViewManipulate(float* view, float length, ImVec2 position, ImVec2 size, ImU32 backgroundColor)
+   bool ViewManipulate(float* view, float length, ImVec2 position, ImVec2 size, ImU32 backgroundColor)
    {
       static bool isDraging = false;
       static bool isClicking = false;
@@ -2852,7 +2852,7 @@ namespace IMGUIZMO_NAMESPACE
                interpolationUp = referenceUp;
             }
             interpolationFrames = 40;
-            
+
          }
          isClicking = false;
          isDraging = false;
@@ -2889,5 +2889,7 @@ namespace IMGUIZMO_NAMESPACE
 
       // restore view/projection because it was used to compute ray
       ComputeContext(svgView.m16, svgProjection.m16, gContext.mModelSource.m16, gContext.mMode);
+
+      return isDraging || (interpolationFrames > 0);
    }
 };
